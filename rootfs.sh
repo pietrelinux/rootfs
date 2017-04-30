578938e6fee9d64370d692ad51d7d053ca674e61
pietrelinux <pietre.linux@gmail.com>
2017-04-30 19:15:28 +0200

    configurando avisos
    c

--- rootfs.sh
+++ rootfs.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-echo ## Instalando dependencias##
+echo " Instalando dependencias"
 apt-get update
 apt-get install -y build-essential bin86 kernel-package libqt4-dev wget libncurses5 libncurses5-dev qt4-dev-tools libqt4-dev zlib1g-dev gcc-arm-linux-gnueabihf git debootstrap u-boot-tools device-tree-compiler libusb-1.0-0-dev android-tools-adb android-tools-fastboot qemu-user-static
-
+echo " creando imagen"
+sleep 3
 dd if=/dev/zero of=rootfs.img bs=1 count=0 seek=3000M
 mkfs.ext4 -b 4096 -F rootfs.img
 mount -o loop rootfs.img /mnt
+echo "iniciando proceso deboostrap"
 debootstrap --arch=armhf --foreign trusty /mnt
 cp /usr/bin/qemu-arm-static /mnt/usr/bin
 cp /etc/resolv.conf /mnt/etc
 > config.sh
 cat <<+ > config.sh
 #!/bin/sh
+echo " configurando debootstrap segunda fase"
 /debootstrap/debootstrap --second-stage
 export LANG=C
 echo "deb http://ports.ubuntu.com/ trusty main restricted universe multiverse" > /etc/apt/sources.list
@@ -30,7 +33,7 @@ ff02::2 ip6-allrouters
 ff02::3 ip6-allhosts" >> /etc/hosts
 echo "auto lo
 iface lo inet loopback" >> /etc/network/interfaces
-echo "/dev/mmcblk0p2 /	   ext4	    errors=remount-ro,noatime,nodiratime 0 1" >> /etc/fstab
+echo "/dev/mmcblk0p1 /	   ext4	    errors=remount-ro,noatime,nodiratime 0 1" >> /etc/fstab
 echo "tmpfs    /tmp        tmpfs    nodev,nosuid,mode=1777 0 0" >> /etc/fstab
 echo "tmpfs    /var/tmp    tmpfs    defaults    0 0" >> /etc/fstab
 sync			
@@ -42,7 +45,7 @@ END
 apt-get update
 apt-get upgrade -y
 apt-get install -y locales dialog software-properties-common makedev isc-dhcp-client ubuntu-minimal ssh cifs-utils screen gedit wireless-tools iw curl libncurses5-dev cpufrequtils rcs aptitude make bc lzop man-db ntp usbutils pciutils lsof most sysfsutils linux-firmware git build-essential autoconf libtool debhelper dh-autoreconf fakeroot pkg-config automake xutils-dev libx11-dev libxext-dev libdrm-dev x11proto-dri2-dev libxfixes-dev lubuntu-desktop onboard vlc
-
+echo "reconfigurando parametros locales"
 locale-gen en_GB.UTF-8
 locale-gen es_ES.UTF-8
 export LC_ALL="es_ES.UTF-8"
