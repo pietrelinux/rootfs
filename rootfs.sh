b99eee0beaa4ca5327870db8cede362180e46963
pietre.linux <pietre.linux@gmail.com>
2017-07-30 04:47:29 +0200

    Update rootfs.sh

--- rootfs.sh
+++ rootfs.sh
@@ -3,16 +3,20 @@ echo " Instalando dependencias"
 sleep 3
 apt-get update
 apt-get install -y build-essential bin86 kernel-package libqt4-dev wget libncurses5 libncurses5-dev qt4-dev-tools libqt4-dev zlib1g-dev gcc-arm-linux-gnueabihf git debootstrap u-boot-tools device-tree-compiler libusb-1.0-0-dev android-tools-adb android-tools-fastboot qemu-user-static
+
 echo " Creando imagen"
 sleep 3
-dd if=/dev/zero of=rootfs.img bs=1 count=0 seek=3000M
-mkfs.ext4 -b 4096 -F rootfs.img
-mount -o loop rootfs.img /mnt
+mkdir /tmp/ramdisk
+mount -t tmpfs none /tmp/ramdisk -o size=1200M
+dd if=/dev/zero of=/tmp/ramdisk/rootfs.img bs=1 count=0 seek=1100M
+mkfs.ext4 -b 4096 -F /tmp/ramdisk/rootfs.img
+chmod 777 /tmp/ramdisk/rootfs.img
+mount -o loop /tmp/ramdisk/rootfs.img /TableX
 echo "Iniciando proceso deboostrap"
 sleep 3
-debootstrap --arch=armhf --foreign trusty /mnt
-cp /usr/bin/qemu-arm-static /mnt/usr/bin
-cp /etc/resolv.conf /mnt/etc
+debootstrap --arch=armhf --foreign trusty /TableX
+cp /usr/bin/qemu-arm-static /TableX/usr/bin
+cp /etc/resolv.conf /TableX/etc
 > config.sh
 cat <<+ > config.sh
 #!/bin/sh
@@ -44,29 +48,32 @@ cat <<END > /etc/apt/apt.conf.d/71-no-recommends
 APT::Install-Recommends "0";
 APT::Install-Suggests "0";
 END
-echo "Actualizando e instalando el sistema"
-sleep 3
+
 apt-get update
-apt-get upgrade -y
-apt-get install -y locales dialog software-properties-common makedev isc-dhcp-client ubuntu-minimal ssh cifs-utils screen gedit wireless-tools iw curl libncurses5-dev cpufrequtils rcs aptitude make bc lzop man-db ntp usbutils pciutils lsof most sysfsutils linux-firmware git build-essential autoconf libtool debhelper dh-autoreconf fakeroot pkg-config automake xutils-dev libx11-dev libxext-dev libdrm-dev x11proto-dri2-dev libxfixes-dev lubuntu-desktop onboard vlc
+
 echo "Reconfigurando parametros locales"
 sleep 3
-locale-gen en_GB.UTF-8
 locale-gen es_ES.UTF-8
 export LC_ALL="es_ES.UTF-8"
 update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
 dpkg-reconfigure locales
 dpkg-reconfigure -f noninteractive tzdata
-passwd
+apt-get install -y lxde
+adduser usuario --disabled-password
+addgroup usuario sudo
 exit
 +
 chmod +x config.sh 
-cp config.sh /mnt/home
+cp config.sh /TableX/home
 echo "Montando directorios"
 sleep 3
-sudo mount -o bind /dev /mnt/dev
-sudo mount -o bind /dev/pts /mnt/dev/pts
-sudo mount -t sysfs /sys /mnt/sys
-sudo mount -t proc /proc /mnt/proc
-chroot /mnt /usr/bin/qemu-arm-static /bin/sh -i ./home/config.sh && exit && umount && /mnt/{sys,proc,dev/pts,dev} && umount /mnt
+sudo mount -o bind /dev /TableX/dev
+sudo mount -o bind /dev/pts /TableX/dev/pts
+sudo mount -t sysfs /sys /TableX/sys
+sudo mount -t proc /proc /TableX/proc
+chroot /TableX /usr/bin/qemu-arm-static /bin/sh -i ./home/config.sh && exit 
+umount /TableX/{sys,proc,dev/pts,dev}
+umount /TableX
+cp  /tmp/ramdisk/rootfs.img /home
+rm config.sh
 exit
